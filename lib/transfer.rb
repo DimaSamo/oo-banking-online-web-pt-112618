9c201719df5832d5dc8857974b5aa453d4cc4282
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-10 08:38:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,11 @@ class Transfer
   end
 
   def execute_transaction
-    @sender.balance -= @amount
-    @receiver.balance += @amount
+    if @sender.valid? && @status == "pending"
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    end
+
   end
 end
