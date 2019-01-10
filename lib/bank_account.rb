02374581c84849eb12331cfa30d4aa2ba66404aa
Dmitri Samotessov <dimasamo96@gmail.com>
2019-01-10 08:28:11 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,5 @@
 class BankAccount
-  attr_reader :name, :balance, :status
+  attr_reader :name, :status
 
   def initialize(name)
     @name = name
@@ -7,4 +7,7 @@ class BankAccount
     @status = "open"
   end
 
+  def deposit(amount)
+    @balanace += amount
+  end
 end
